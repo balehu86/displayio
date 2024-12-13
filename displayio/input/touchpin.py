4b2d1b833be482392e7075f38566e7c515262a77
balehu <balehu86@gmail.com>
2024-12-13 12:12:50 +0800

    update input

--- displayio/input/touchpin.py
+++ displayio/input/touchpin.py
@@ -21,7 +21,6 @@ class TouchPin(Input):
         current_time = time.ticks_ms()
 
         if touch_value > self.touch_threshold:# 触摸按下
-            
             if self.state == self.IDLE:# 第一次检测到按下,将返回 PRESS event
                 self.state = self.PRESS
                 self.press_start_time = current_time
@@ -48,19 +47,21 @@ class TouchPin(Input):
 
             if self.state == self.PRESS:
                 # 触摸持续时间
-                press_duration = time.ticks_diff(current_time, self.press_start_time)         
+                press_duration = time.ticks_diff(current_time, self.press_start_time)
+                # 触摸间隔
+                click_interval = time.ticks_diff(self.press_start_time, self.last_release_time)
+                # 重置状态
+                self.last_release_time = current_time
+                self.state = self.IDLE
                 # 有效单次点击检测
                 if self.click_min_duration < press_duration < self.click_max_duration:
                     # 先判断是否为双击
-                    click_interval = time.ticks_diff(self.press_start_time, self.last_release_time)
-                    self.last_release_time = current_time
-                    self.state = self.IDLE
                     if click_interval <= self.double_click_max_interval:# 是双击
                         return Event(self.DOUBLE_CLICK, target_widget=self.target_widget, 
                                      target_position=self.target_position)
                     else:# 不是双击
                         return Event(self.CLICK, target_widget=self.target_widget, 
                                      target_position=self.target_position)
-                else: # 持续时长超过短按,不足长按
+                else: # 持续时长 (超过短按,不足长按) 或 (不足短按)
                     return Event(self.RELEASE, target_widget=self.target_widget, 
                                      target_position=self.target_position)
