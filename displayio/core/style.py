59802ac0fb44db5854a3d647e10d82de3069f07c
balehu <balehu86@gmail.com>
2025-01-19 21:25:53 +0800

    Update style.py

--- displayio/core/style.py
+++ displayio/core/style.py
@@ -1,6 +1,13 @@
 # ./core/style.py
 from .bitmap import Bitmap
 
+class Background:
+    def __init__(self, color=None, pic=None):
+        if color is None and pic is None:
+            raise ValueError('Background 类初始化错误, color 和 pic 参数必须二选一')
+        self.color = color
+        self.pic = pic
+
 class Color:
     __slots__ = ()
     RED   = 0xf800
@@ -38,5 +45,5 @@ class Style:
     ALIGN_END = 'end'
     
     # 方向
-    HORIZONTAL = 0
-    VERTICAL = 1
+    HORIZONTAL = 'horizontal'
+    VERTICAL = 'vertical'
